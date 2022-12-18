4ee2c5524aa484b0d2157fb8d3764dd6fa07e844
KramerEd <danat7531@gmail.com>
2022-12-18 16:10:51 +0200

    calendar

--- src/components/CustomCalendar.tsx
+++ src/components/CustomCalendar.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
 import {
-  Button,
   Modal,
   useColorScheme,
-  View,
   TouchableOpacity,
+  Button,
+  Text,
 } from 'react-native';
 import {Calendar} from 'react-native-calendars';
 import Palette from '../styles/Palette';
@@ -17,36 +17,47 @@ const customCalendar = props => {
     setCalendarModalModal(false);
   };
 
+  const data = new Date();
+  const day: number = data.getDate();
+
+  const [testDate, setTestDate] = useState(day);
   return (
-    <Modal visible={calendarModal} transparent={true} animationType="fade">
-      <TouchableOpacity
-        onPress={closeModal}
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          backgroundColor: 'rgba(218, 14, 14, 0.3)',
-        }}>
-        <Calendar
-          disableAllTouchEventsForDisabledDays={true}
-          {...props}
+    <>
+      <Modal visible={calendarModal} transparent={true} animationType="fade">
+        <TouchableOpacity
+          // onPress={closeModal}
           style={{
-            borderRadius: 15,
-            margin: 12,
-            elevation: 5,
-          }}
-          theme={{
-            calendarBackground: scheme
-              ? Palette.darkTh.backGround
-              : Palette.lightTh.backGround,
-            dayTextColor: scheme ? Palette.darkTh.text : Palette.lightTh.text,
-            textDisabledColor: scheme ? '#555' : '#999',
-            monthTextColor: scheme ? Palette.darkTh.text : Palette.lightTh.text,
-            arrowColor: 'red',
-            selectedDayBackgroundColor: 'red',
-          }}
-        />
-      </TouchableOpacity>
-    </Modal>
+            flex: 1,
+            justifyContent: 'center',
+            backgroundColor: 'rgba(218, 14, 14, 0.3)',
+          }}>
+          <Calendar
+            disableAllTouchEventsForDisabledDays={true}
+            {...props}
+            style={{
+              borderRadius: 15,
+              margin: 12,
+              elevation: 5,
+            }}
+            theme={{
+              calendarBackground: scheme
+                ? Palette.darkTh.backGround
+                : Palette.lightTh.backGround,
+              dayTextColor: scheme ? Palette.darkTh.text : Palette.lightTh.text,
+              textDisabledColor: scheme ? '#555' : '#999',
+              monthTextColor: scheme
+                ? Palette.darkTh.text
+                : Palette.lightTh.text,
+              arrowColor: 'red',
+              selectedDayBackgroundColor: 'red',
+            }}
+            onDayPress={day => setTestDate(day.day)}
+          />
+          <Button title="close" onPress={closeModal} />
+          <Text style={{fontSize: 24, color: 'white'}}>{testDate}</Text>
+        </TouchableOpacity>
+      </Modal>
+    </>
   );
 };
 
