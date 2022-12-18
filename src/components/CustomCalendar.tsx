04c2eb8d9e6596da11426629ee1e8784e4ae1d22
KramerEd <danat7531@gmail.com>
2022-12-18 15:05:11 +0200

    calendar

--- src/components/CustomCalendar.tsx
+++ src/components/CustomCalendar.tsx
@@ -1,36 +1,38 @@
 import React, {useState} from 'react';
-import {Button, Modal, useColorScheme, View} from 'react-native';
+import {
+  Button,
+  Modal,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 import Palette from '../styles/Palette';
 
 const customCalendar = props => {
   const scheme = useColorScheme() === 'dark';
-
-  const [openModal, setOpenModal] = useState(!false);
+  const [calendarModal, setCalendarModalModal] = useState(!false);
 
   const closeModal = () => {
-    console.log('close');
-    setOpenModal(false);
+    setCalendarModalModal(false);
   };
 
   return (
-    <Modal visible={openModal} transparent={true} animationType="fade">
-      <View
+    <Modal visible={calendarModal} transparent={true} animationType="fade">
+      <TouchableOpacity
+        onPress={closeModal}
         style={{
           flex: 1,
           justifyContent: 'center',
-          padding: 40,
-          backgroundColor: 'rgba(100, 100, 100, 0.6)',
+          backgroundColor: 'rgba(218, 14, 14, 0.3)',
         }}>
         <Calendar
           disableAllTouchEventsForDisabledDays={true}
           {...props}
           style={{
-            borderRadius: 5,
+            borderRadius: 15,
             margin: 12,
             elevation: 5,
-            borderWidth: 4,
-            borderColor: 'rgba(100, 100, 100, 0.2)',
           }}
           theme={{
             calendarBackground: scheme
@@ -39,10 +41,11 @@ const customCalendar = props => {
             dayTextColor: scheme ? Palette.darkTh.text : Palette.lightTh.text,
             textDisabledColor: scheme ? '#555' : '#999',
             monthTextColor: scheme ? Palette.darkTh.text : Palette.lightTh.text,
+            arrowColor: 'red',
+            selectedDayBackgroundColor: 'red',
           }}
         />
-      </View>
-      <Button title="close" onPress={closeModal} />
+      </TouchableOpacity>
     </Modal>
   );
 };
